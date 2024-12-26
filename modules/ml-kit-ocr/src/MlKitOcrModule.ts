fdc0851360b23596521269fd2425dce6849fd30c
Michael Wood <esiotrot@gmail.com>
2024-12-26 15:59:44 +0200

    Remove unused import of nonexistent types

--- modules/ml-kit-ocr/src/MlKitOcrModule.ts
+++ modules/ml-kit-ocr/src/MlKitOcrModule.ts
@@ -1,10 +1,8 @@
 import { NativeModule, requireNativeModule } from 'expo';
 
-import { MlKitOcrModuleEvents } from './MlKitOcr.types';
-
 declare class MlKitOcrModule extends NativeModule {
   recognizeTextAsync(uri: string): Promise<string>;
 }
 
- // This call loads the native module object from the JSI.
+// This call loads the native module object from the JSI.
 export default requireNativeModule<MlKitOcrModule>('MlKitOcr');
